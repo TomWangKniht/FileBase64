092fc3e743a1d976200bf5a6fcf86cf4025f72f1
TomWangKniht <tomwangkniht@gmail.com>
2016-10-14 16:38:17 +0800

    移除不必要的程式

--- File64/File64/Controllers/HomeController.cs
+++ File64/File64/Controllers/HomeController.cs
@@ -20,11 +20,5 @@ namespace File64.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
